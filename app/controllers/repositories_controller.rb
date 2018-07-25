1d07bb5e1502a5690efa4f08cbea71ef70af05ca
Kim Foukaris <kimfoukaris@outlook.com>
2018-07-25 13:39:48 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -13,7 +13,7 @@ class RepositoriesController < ApplicationController
       end
       body = JSON.parse(@resp.body)
       if @resp.success?
-        @venues = body["response"]["items"]
+        @items = body["items"]
       else
         @error = body["meta"]["errorDetail"]
       end
